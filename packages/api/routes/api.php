af4de44c87cf91b2b9bf2e4e3b61f6ad775f5190
MatheusTheGeek <miliandroxd@gmail.com>
2021-07-15 16:09:19 -0300

    api: request authenticated user router added

--- packages/api/routes/api.php
+++ packages/api/routes/api.php
@@ -11,5 +11,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', fn () => auth()->user());
+
     Route::post('/logout', Logout::class);
 });
